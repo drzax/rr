5b54c730bf1bf4dfcebdaed3a81ee8c0e536895a
Simon Elvery <simon@elvery.net>
2018-11-10 12:01:05 +1000

    Add manifest and sw stub
    Fixes #3

--- aunty.config.js
+++ aunty.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
+const { GenerateSW } = require("workbox-webpack-plugin");
+
 module.exports = {
   type: "react",
   devServer: {
@@ -16,6 +18,16 @@ module.exports = {
         FB_MESSGING_SENDER_ID: JSON.stringify(process.env.FB_MESSGING_SENDER_ID)
       })
     );
+    config.plugins.push(
+      new GenerateSW({
+        // importWorkboxFrom: "local",
+        // these options encourage the ServiceWorkers to get in there fast
+        // and not allow any straggling "old" SWs to hang around
+        clientsClaim: true,
+        skipWaiting: true
+      })
+    );
+
     return config;
   },
   babel: config => {
--- src/index.js
+++ src/index.js
@@ -27,3 +27,16 @@ if (process.env.NODE_ENV === "development") {
   log.setLevel("debug");
   log.debug(`[${PROJECT_NAME}] public path: ${__webpack_public_path__}`);
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(registration => {
+        console.log("SW registered: ", registration);
+      })
+      .catch(registrationError => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
