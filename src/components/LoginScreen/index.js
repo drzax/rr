b102b088a3fc51ca0adeed361dfa55d2fe57c9e9
Simon Elvery <simon@elvery.net>
2018-11-17 18:27:18 +1000

    Give login a notifications option

--- src/components/LoginScreen/index.js
+++ src/components/LoginScreen/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./styles.scss";
-
+import {
+  NotificationsProvider,
+  NotificationsConsumer
+} from "../Notifications/context";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -9,7 +12,6 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import LoginButtonEmail from "../LoginButtonEmail";
 import LoginButtonAnon from "../LoginButtonAnon";
-import { NotificationsConsumer } from "../Notifications/context";
 import * as log from "loglevel";
 
 export default class LoginScreen extends React.Component {
@@ -27,42 +29,44 @@ export default class LoginScreen extends React.Component {
     const { email } = this.state;
 
     return (
-      <div>
-        <Dialog
-          open={true}
-          aria-labelledby="form-dialog-title"
-          BackdropProps={{ invisible: true }}
-        >
-          <DialogTitle id="form-dialog-title">Sign-in or sign-up</DialogTitle>
+      <div className={styles.root}>
+        <NotificationsProvider>
+          <Dialog
+            open={true}
+            aria-labelledby="form-dialog-title"
+            BackdropProps={{ invisible: true }}
+          >
+            <DialogTitle id="form-dialog-title">Sign-in or sign-up</DialogTitle>
 
-          <DialogContent>
-            <DialogContentText>
-              All we need to make it permanent is your email address.
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="email"
-              name="email"
-              label="Email address"
-              type="email"
-              variant="outlined"
-              value={email}
-              onChange={this.handleInputChange}
-              fullWidth
-            />
-          </DialogContent>
-          <NotificationsConsumer>
-            {({ open }) => {
-              return (
-                <DialogActions>
-                  <LoginButtonAnon notify={open} />
-                  <LoginButtonEmail notify={open} email={email} />
-                </DialogActions>
-              );
-            }}
-          </NotificationsConsumer>
-        </Dialog>
+            <DialogContent>
+              <DialogContentText>
+                All we need to make it permanent is your email address.
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="email"
+                name="email"
+                label="Email address"
+                type="email"
+                variant="outlined"
+                value={email}
+                onChange={this.handleInputChange}
+                fullWidth
+              />
+            </DialogContent>
+            <NotificationsConsumer>
+              {({ open }) => {
+                return (
+                  <DialogActions>
+                    <LoginButtonAnon notify={open} />
+                    <LoginButtonEmail notify={open} email={email} />
+                  </DialogActions>
+                );
+              }}
+            </NotificationsConsumer>
+          </Dialog>
+        </NotificationsProvider>
       </div>
     );
   }
